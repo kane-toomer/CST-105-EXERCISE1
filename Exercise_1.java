cf4eda2598c428ed5213527e23126e921091467b
Kane Toomer <kanetoomer@yahoo.com>
2023-07-22 18:37:05 -0700

    editted a comment

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
 
 public class Exercise_1 {
-	
+
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		
+
 		// Tell the user to enter a 5-digit positive integer
 		System.out.print("Enter a 5-digit positive integer: ");
-		
+
 		int number = scnr.nextInt();
-		
+
 		// Compute each individual digit
 		int digit1 = number % 10;
 		number /= 10;
-		
+
 		int digit2 = number % 10;
 		number /= 10;
-		
+
 		int digit3 = number % 10;
 		number /= 10;
-		
+
 		int digit4 = number % 10;
 		number /= 10;
-		
+
 		int digit5 = number % 10;
 		number /= 10;
-		
+
 		// Calculate and display the sum of the digits
-        int sum = digit1 + digit2 + digit3 + digit4 + digit5;
-        System.out.println("The sum of the digits is " + digit5 + " + " + digit4 + " + " + digit3 + " + " + digit2 + " + " + digit1 + " = " + sum);
-	
+		int sum = digit1 + digit2 + digit3 + digit4 + digit5;
+
+		// Display the sum of the digits
+		System.out.println("The sum of the digits is " + digit5 + " + " + digit4 + " + " + digit3 + " + " + digit2
+				+ " + " + digit1 + " = " + sum);
+
 	}
 }
